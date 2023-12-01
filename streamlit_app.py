22486bae661ef6ea5d34077bc25ce3c3d969a84a
mary-ka-cons <vergez.mariecamille@gmail.com>
2023-12-01 12:20:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ import streamlit as st
 from PIL import Image
 
 ### Images
-img = Image.open("/Users/marie-camillevergez/Documents/GitHub/streamlit-example/Images/mario.png")
-st.image(img)
+#img = Image.open("/Users/marie-camillevergez/Documents/GitHub/streamlit-example/Images/mario.png")
+#st.image(img)
 
 
 
