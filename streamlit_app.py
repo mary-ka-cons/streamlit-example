d08d537f1d5fa4b7e0f303967c47235b59b03d29
Marie-Camille Vergez <vergez.mariecamille@gmail.com>
2024-01-05 15:32:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ def main():
         """
         <style>
             .stApp {
-                background-color: #132946;
+                background-color: #1B3735;
             }
         </style>
         """,
